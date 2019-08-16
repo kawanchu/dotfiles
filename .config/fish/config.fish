9b2a5b445ab6d0ef55222bc7f98b14a1f46f4b2e
Shinpei Kawahito <kawahito.shinpei@gmail.com>
2019-08-16 20:55:14 +0900

    fix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,11 @@
 status --is-interactive; and source (anyenv init -|psub)
-alias g='git'
+
+alias g="git"
+
+set -g fish_user_paths "/usr/local/opt/gettext/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/qt@5.5/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/openssl/bin" $fish_user_paths
+set -gx LC_ALL en_US.UTF-8
+set -gx LIBRARY_PATH "/usr/local/opt/openssl/lib/" $LIBRARY_PATH
+set -gx LDFLAGS "-L/usr/local/opt/openssl/lib"
+set -gx CPPFLAGS "-I/usr/local/opt/openssl/include"
