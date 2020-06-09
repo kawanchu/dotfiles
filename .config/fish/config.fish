369aa3650571e3fdea4f4ae50cf8e5d7b4f7aa78
Shinpei Kawahito <kawahito.shinpei@gmail.com>
2020-06-09 13:10:57 +0900

    fix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,21 +5,24 @@ alias vim="nvim"
 
 set -g fish_user_paths "/usr/local/opt/gettext/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/ncurses/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/openssl/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/openssl@1.1/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/qt/bin" $fish_user_paths
 
 set -gx LC_ALL en_US.UTF-8
-set -gx LIBRARY_PATH "/usr/local/opt/openssl/lib/" $LIBRARY_PATH
+set -gx LIBRARY_PATH "/usr/local/opt/openssl@1.1/lib/" $LIBRARY_PATH
 set -gx LDFLAGS "-L/usr/local/opt/ncurses/lib" $LDFLAGS
-set -gx LDFLAGS "-L/usr/local/opt/openssl/lib" $LDFLAGS
+set -gx LDFLAGS "-L/usr/local/opt/openssl@1.1/lib" $LDFLAGS
 set -gx LDFLAGS "-L/usr/local/opt/readline/lib" $LDFLAGS
 set -gx CPPFLAGS "-I/usr/local/opt/ncurses/include" $CPPFLAGS
-set -gx CPPFLAGS "-I/usr/local/opt/openssl/include" $CPPFLAGS
+set -gx CPPFLAGS "-I/usr/local/opt/openssl@1.1/include" $CPPFLAGS
 set -gx CPPFLAGS "-I/usr/local/opt/readline/include" $CPPFLAGS
+set -gx PATH "/Users/kawahito.shinpei/Applications/fabric-samples/bin" $PATH
+set -gx PATH "/Users/kawahito.shinpei/.ghq/github.com/flutter/flutter/bin" $PATH
 
 function brew
-  set -l index (contains -i $HOME/.anyenv/envs/pyenv/shims $PATH)
-  set PATH[$index] /bin
+  set -xl PATH $PATH
+  if type -q pyenv; and contains (pyenv root)/shims $PATH
+    set -e PATH[(contains -i (pyenv root)/shims $PATH)]
+  end
   command brew $argv
-  set PATH[$index] $HOME/.anyenv/envs/pyenv/shims
 end
